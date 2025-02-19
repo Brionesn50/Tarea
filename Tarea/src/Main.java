b87771ecccd93eaddefc9bf44f70c02d69380a6d
Brionesn50 <nicotestaplicaciones@gmail.com>
2025-02-19 20:10:47 -0300

    actualizacion de comentarios

--- Tarea/src/Main.java
+++ Tarea/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Prueba test");
         System.out.println("HOla");
+        System.out.println("adios");
     }
 }
