76841a74913e8d94b85835151f63bad509ebb521
Brionesn50 <nicotestaplicaciones@gmail.com>
2025-02-19 20:00:44 -0300

    cambio en test

--- Tarea/src/Main.java
+++ Tarea/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Prueba");
+        System.out.println("Prueba test");
+        System.out.println("HOla");
     }
 }
